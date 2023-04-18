b5fda484bb468d637d95cafb1f176026733b75ba
riamxpp <riamstefeson616@gmail.com>
2023-04-18 14:37:43 -0300

    Criando estrutura do projeto, adicionando dependências e testando as dependências

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+  include_once('config/url.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Agenda</title>
+  <!-- bootstrap -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/css/bootstrap.min.css">
+  <!-- font-awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+  <!-- CSS -->
+  <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
+</head>
+<body>
+  <h1>Teste agenda</h1>
+  <i class="fas fa-eye"></i>
+</body>
+</html>
