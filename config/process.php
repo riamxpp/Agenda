01cf1e5cc43bd410055fa256d2a18c389a374ee6
riamxpp <riamstefeson616@gmail.com>
2023-04-19 15:39:51 -0300

    Adicionando funcionalidade para criar um novo contato e fechando a conexão com o bd, após a criação redirecionamos o user para a home

--- config/process.php
+++ config/process.php
@@ -5,27 +5,55 @@
   include_once('connection.php');
   include_once('url.php');
 
-  // RETORNA UM CONTATO
-  // $contact
-  if(!empty($_GET)){
-    $id = $_GET['id'];
-  }
-  if (!empty($id)){ 
-    $query_contact = 'SELECT * FROM contacts WHERE id = :id';
-    $stmt = $conn->prepare($query_contact);
-    $stmt->bindParam('id', $id);
-    $stmt->execute();
 
-    $contact = $stmt->fetch();
+  $data = $_POST;
 
+  if (!empty($data)){
+    // CRIAR CONTATO
+    if($data['type'] == 'create'){
+      $name = $data['name'];
+      $observations = $data['observations'];
+      $phone = $data['phone'];
+      
+      $query = 'INSERT INTO contacts (name, observations, phone) VALUES (:name, :observations, :phone)';
+      $stmt = $conn->prepare($query);
+      $stmt->bindParam(':name', $name);
+      $stmt->bindParam(':observations', $observations);
+      $stmt->bindParam(':phone', $phone);
+
+      try {
+        $stmt->execute();
+        $_SESSION['msg'] = "Contato criado com sucesso!";
+      }catch(Exception $e) {
+        echo 'ERRO: ' . $e->getMessage() . '<br>';
+      }
+    }
+    // REDIRET HOME
+    header('Location: ' . $BASE_URL . '../index.php');
   }else {
-    // RETORNA TODOS OS CONTATOS
-    $contacts = [];
-    $query = "SELECT * FROM contacts";
+    // RETORNA UM CONTATO
+    if(!empty($_GET)){
+      $id = $_GET['id'];
+    }
+    if (!empty($id)){ 
+      $query_contact = 'SELECT * FROM contacts WHERE id = :id';
+      $stmt = $conn->prepare($query_contact);
+      $stmt->bindParam('id', $id);
+      $stmt->execute();
 
-    $stmt = $conn->prepare($query);
+      $contact = $stmt->fetch();
 
-    $stmt->execute(); 
-    $contacts = $stmt->fetchAll();
+    }else {
+      // RETORNA TODOS OS CONTATOS
+      $contacts = [];
+      $query = 'SELECT * FROM contacts';
+
+      $stmt = $conn->prepare($query);
+
+      $stmt->execute(); 
+      $contacts = $stmt->fetchAll();
+    }
   }
 
+  #FECHANDO CONEXÃO
+  $conn = null;
