024c6844815f68a4b5e958930ace6eba8c100a6f
riamxpp <riamstefeson616@gmail.com>
2023-04-19 14:11:03 -0300

    Criando a página de adicionar novo contato

--- create.php
+++ create.php
@@ -1,8 +1,26 @@
 <?php 
   include_once('templates/header.php');
 ?>
-
-  <h1>Create</h1>
+  <div class="container">
+    <?php include_once('templates/backbtn.html'); ?>
+    <h1 id="main-title">Criando contato</h1>
+    <form id="create-form" action="<?= $BASE_URL ?>config/process.php" method="POST">
+      <input type="hidden" name="type" value="create">
+      <div class="form-group">
+        <label for="name">Nome do contato:</label>
+        <input type="text" name="name" required class="form-control" placeholder="Insira o Nome" id="name">
+      </div>
+      <div class="form-group">
+        <label for="phone">Telefone do contato:</label>
+        <input type="text" name="phone" required class="form-control" placeholder="Insira o Número" id="phone">
+      </div>
+      <div class="form-group">
+        <label for="observations">Observações:</label>
+        <textarea type="text" name="observations" required class="form-control" placeholder="Insira as observações" id="observations" rows="3"></textarea>
+      </div><br>
+      <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+  </div>
 
 <?php 
   include_once('templates/footer.php');
