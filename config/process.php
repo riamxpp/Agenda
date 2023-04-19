d2ea91b6ad8962c17258a6306da102b491bba6a0
riamxpp <riamstefeson616@gmail.com>
2023-04-19 13:36:36 -0300

    Tela de registro de contato

--- config/process.php
+++ config/process.php
@@ -5,10 +5,27 @@
   include_once('connection.php');
   include_once('url.php');
 
-  $contacts = [];
-  $query = "SELECT * FROM contacts";
+  // RETORNA UM CONTATO
+  // $contact
+  if(isset($_GET)){
+    $id = $_GET['id'];
+  }
+  if (!empty($id)){ 
+    $query_contact = 'SELECT * FROM contacts WHERE id = :id';
+    $stmt = $conn->prepare($query_contact);
+    $stmt->bindParam('id', $id);
+    $stmt->execute();
 
-  $stmt = $conn->prepare($query);
+    $contact = $stmt->fetch();
 
-  $stmt->execute(); 
-  $contacts = $stmt->fetchAll();
+  }else {
+    // RETORNA TODOS OS CONTATOS
+    $contacts = [];
+    $query = "SELECT * FROM contacts";
+
+    $stmt = $conn->prepare($query);
+
+    $stmt->execute(); 
+    $contacts = $stmt->fetchAll();
+  }
+
