c0f6885abfea473a6a12dae17a96bcfda41fed3f
riamxpp <riamstefeson616@gmail.com>
2023-04-19 11:00:44 -0300

    Criando a página inicial

--- config/process.php
+++ config/process.php
@@ -5,12 +5,10 @@
   include_once('connection.php');
   include_once('url.php');
 
+  $contacts = [];
   $query = "SELECT * FROM contacts";
 
   $stmt = $conn->prepare($query);
 
   $stmt->execute(); 
   $contacts = $stmt->fetchAll();
-
-  print_r($contacts);
-  
--- index.php
+++ index.php
@@ -2,9 +2,44 @@
   include_once('templates/header.php');
 ?>
 
-  <h1>Teste agenda</h1>
-  <i class="fas fa-eye"></i>
-
+  <div class='container'>
+    <?php if(isset($printMsg) && $printMsg != ''): ?>
+      <p id="msg"><?= $printMsg ?></p>
+    <?php endif; ?>
+    <h1 id="main-title">Minha agenda</h1>
+    <?php if(count($contacts) > 0): ?>
+      <table class="table" id="contacts-table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Telefone</th>
+            <th scope="col">#</th>
+          </tr>
+        </thead>  
+        <tbody>
+          <?php foreach($contacts as $contact): ?>
+            <tr>
+              <td scope="row"><?= $contact['id'] ?></td>
+              <td scope="row"><?= $contact['name'] ?></td>
+              <td scope="row"><?= $contact['phone'] ?></td>
+              <td class="actions">
+                <a href="#">
+                  <i class="fas fa-eye check-icon"></i>
+                  <i class="far fa-edit edit-icon"></i>
+                  <button type="submit">
+                    <i class="fas fa-times delete-icon"></i>
+                  </button>
+                </a>
+              </td>
+            </tr>
+          <?php endforeach; ?>
+        </tbody>
+      </table>
+    <?php else: ?>
+      <p id="empty-list-text">Não tem contatos, <a href="<?= $BASE_URL ?>create.php">adicione um novo contato</a></p>
+    <?php endif; ?>
+  </div>
 <?php 
   include_once('templates/footer.php');
 ?>
